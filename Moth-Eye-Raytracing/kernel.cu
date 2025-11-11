849a024b881789d9d330c9bc44a1a2a14ce7496f
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-11-11 00:01:47 -0500

    Modified the scene
    - Modified the scene to test the new light sources

--- Moth-Eye-Raytracing/kernel.cu
+++ Moth-Eye-Raytracing/kernel.cu
@@ -12,6 +12,8 @@
 #include <functional>
 #include "PointSource.h"
 #include "QuantumDot.h"
+#include "DirectionalLight.h"
+#include "ConeLight.h"
 
 void CaptureTest()
 {
@@ -25,16 +27,15 @@ void CaptureTest()
 	scene.AddObject(new Wave(-10, -5.5, 10, -5.5, 100, [](double x, double y) { return 1.2; }));
 	scene.AddObject(new Wave(-10, -6, 10, -6, 100, [](double x, double y) { return 1.3; }));
 	scene.AddObject(new Wave(-10, -6.5, 10, -6.5, 100, [](double x, double y) { return 1.4; }));
-	scene.AddObject(new QuantumDot(-3, 3, 3, 100));
-	scene.AddObject(new QuantumDot(3, 3, 3, 100));
+	//scene.AddObject(new QuantumDot(-3, 3, 3, 100));
+	//scene.AddObject(new QuantumDot(3, 3, 3, 100));
 
-	//scene.AddRay(Ray(1.0, 0.0, 0.0, -1.0));
-	//scene.AddRay(Ray(2.0, 0.0, -1.0, -1.0));
-	//scene.AddRay(Ray(3.0, 0.0, 1.0, -1.0));
+	//PointSource source = PointSource(0, 0, 1000);
+	//DirectionalLight light = DirectionalLight(0.0, 0.0, 5.0, 5.0, 1000);
+	ConeLight light = ConeLight(0.0, 0.0, -1, 0, 0, -1, 1000);
 
-	PointSource source = PointSource(0, 0, 10000);
-
-	scene.AddRays(source.GenerateRays());
+	//scene.AddRays(source.GenerateRays());
+	scene.AddRays(light.GenerateRays());	
 	std::cout << "Render" << std::endl;
 	scene.Render();
 }
