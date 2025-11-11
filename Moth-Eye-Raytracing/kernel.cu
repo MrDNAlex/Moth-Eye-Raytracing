575f88212914ff58f3579648f6b82a1036992c72
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-11-11 02:31:26 -0500

    Setup of Scenes
    - Setup the QD Unit Cell Scene
    - Setup the Cone Source Unit Cell Scene

--- Moth-Eye-Raytracing/kernel.cu
+++ Moth-Eye-Raytracing/kernel.cu
@@ -15,6 +15,107 @@
 #include "DirectionalLight.h"
 #include "ConeLight.h"
 
+std::vector<double> linspace(double start, double end, int num) {
+	std::vector<double> result;
+	if (num <= 0) return result;
+	if (num == 1) {
+		result.push_back(start);
+		return result;
+	}
+
+	double step = (end - start) / (num - 1);
+	for (int i = 0; i < num; ++i)
+		result.push_back(start + step * i);
+
+	return result;
+}
+
+void ConeWaveUnitCell(int QDs)
+{
+	std::string name = "Cone" + std::to_string(QDs) + "WaveUnitCell";
+
+	Scene scene = Scene(name);
+
+	//Constants
+	double qdRadius = 10.0;
+	double mothEyeHeight = 250.0;
+	double pi = 3.14159265358979323846;
+
+	double A = mothEyeHeight / 2.0;
+	double B = 2.0 * pi / mothEyeHeight;
+	double C = mothEyeHeight / 4.0;
+	double D = mothEyeHeight / 2.0;
+
+	double startX = 0.0;
+	double endX = 250.0;
+	double startY = 0.0;
+	double endY = 0.0;
+	double depth = -200.0;
+
+	scene.AddObject(new Wave(startX, startY, endX, endY, 1000, [](double x, double y) { return 1.1; }, A, B, C, D));
+	scene.AddObject(new Mirror(startX, startY, startX, depth));
+	scene.AddObject(new Mirror(endX, startY, endX, depth));
+	scene.AddObject(new Target(startX, depth, endX, depth));
+
+	std::vector<double> qdPositionsX = linspace(startX, endX, QDs + 2);
+
+	for (int i = 1; i < qdPositionsX.size() - 1; i++)
+	{
+		double ox = qdPositionsX[i];
+		double oy = -100;
+
+		double ax = startX;
+		double ay = startY;
+
+		double bx = endX;
+		double by = endY;
+
+		scene.AddRaySource(new ConeLight(ox, oy, ax, ay, bx, by, 10));
+	}
+
+	std::cout << "Render" << std::endl;
+	scene.Render();
+}
+
+
+void QDInternalWaveUnitCell(int QDs)
+{
+	std::string name = "QDInternal" + std::to_string(QDs) + "WaveUnitCell";
+
+	Scene scene = Scene(name);
+	
+	//Constants
+	double qdRadius = 10.0;
+	double mothEyeHeight = 250.0;
+	double pi = 3.14159265358979323846;
+
+	double A = mothEyeHeight / 2.0;
+	double B = 2.0 * pi / mothEyeHeight;
+	double C = mothEyeHeight / 4.0;
+	double D = mothEyeHeight / 2.0;
+
+	double startX = 0.0;
+	double endX = 250.0;
+	double startY = 0.0;
+	double endY = 0.0;
+	double depth = -200.0;
+
+	scene.AddObject(new Wave(startX, startY, endX, endY, 1000, [](double x, double y) { return 1.1; }, A, B, C, D));
+	scene.AddObject(new Mirror(startX, startY, startX, depth));
+	scene.AddObject(new Mirror(endX, startY, endX, depth));
+	scene.AddObject(new Target(startX, depth, endX, depth));
+
+	std::vector<double> qdPositionsX = linspace(startX, endX, QDs+2);
+
+	for (int i = 1; i < qdPositionsX.size()-1; i++)
+	{
+		scene.AddObject(new QuantumDot(qdPositionsX[i], -100, qdRadius, 1000));
+		scene.AddRaySource(new PointSource(qdPositionsX[i], -100, 1000));
+	}
+
+	std::cout << "Render" << std::endl;
+	scene.Render();
+}
 
 
 void CaptureTest()
@@ -44,7 +145,16 @@ void CaptureTest()
 
 int main()
 {
-	CaptureTest();
+	//CaptureTest();
+	QDInternalWaveUnitCell(1);
+	QDInternalWaveUnitCell(2);
+	QDInternalWaveUnitCell(3);
+	QDInternalWaveUnitCell(4);
+
+	ConeWaveUnitCell(1);
+	ConeWaveUnitCell(2);
+	ConeWaveUnitCell(3);
+	ConeWaveUnitCell(4);
 
 	std::cout << "Press ENTER to exit...";
 	std::cin.get();
