6559c7ae813b0ef6423c441a27fe965b8de679a7
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-04-06 16:29:55 -0400

    Added the execution bash file
    - Added a Shell file that executes all the calculations at the same time

---
+++ Execute.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+nohup ./Moth-Eye-Raytracing 1 > log1.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 0 > log2_0.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 5 > log2_5.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 10 > log2_10.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 15 > log2_15.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 20 > log2_20.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 40 > log2_40.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 45 > log2_45.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 50 > log2_50.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 55 > log2_55.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 2 60 > log2_60.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 0 > log3_0.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 5 > log3_5.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 10 > log3_10.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 15 > log3_15.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 20 > log3_20.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 40 > log3_40.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 45 > log3_45.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 50 > log3_50.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 55 > log3_55.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 3 60 > log3_60.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 4 > log4.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 0 > log5_0.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 5 > log5_5.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 10 > log5_10.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 15 > log5_15.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 20 > log5_20.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 40 > log5_45.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 45 > log5_45.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 50 > log5_50.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 55 > log5_55.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 5 60 > log5_60.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 0 > log6_0.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 5 > log6_5.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 10 > log6_10.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 15 > log6_15.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 20 > log6_20.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 40 > log6_40.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 45 > log6_45.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 50 > log6_50.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 55 > log6_55.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 6 60 > log6_60.txt 2>&1 &
+nohup ./Moth-Eye-Raytracing 7 > log7.txt 2>&1 &
